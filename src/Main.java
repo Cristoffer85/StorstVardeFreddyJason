f9e9087e99bed043e349486c914d5ec37e99c5e4
Cristoffer <119351534+Cristoffer85@users.noreply.github.com>
2023-03-15 11:57:19 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int Freddy = 42;
+        int Jason = 121;
+        int FreddyIkapp = Jason - Freddy;
+
+        System.out.println (Freddy);
+        System.out.println (Jason);
+        System.out.println ("Freddy måste döda " + FreddyIkapp + "st personer för att komma ikapp Jason");
+
+
+        /// Skriv din kod här
+    }
+}
